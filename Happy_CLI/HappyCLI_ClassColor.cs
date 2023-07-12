7f608ba3a709088b575a59c0c46c4745266ddcd6
nima <mohammadi.ni@outlook.com>
2023-07-12 23:47:33 +0330

    Changing the name of the color text class. Writing part of the methods of the ColorText class.

--- Happy_CLI/HappyCLI_ClassColor.cs
+++ Happy_CLI/HappyCLI_ClassColor.cs
@@ -6,8 +6,52 @@ using System.Threading.Tasks;
 
 namespace Happy_CLI
 {
-    public class Colors
+    public class ColorText
     {
+        public ColorText() { }
+        public void write(string text,ConsoleColor fcolor) 
+        {
+            Console.ForegroundColor = fcolor;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+        public void write(string text, ConsoleColor fcolor,ConsoleColor bcolor)
+        {
+            Console.ForegroundColor = fcolor;
+            Console.BackgroundColor = bcolor;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+        public void writeLine(string text, ConsoleColor fcolor)
+        {
+            Console.ForegroundColor = fcolor;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+        public void writeLine(string text, ConsoleColor fcolor, ConsoleColor bcolor)
+        {
+            Console.ForegroundColor = fcolor;
+            Console.BackgroundColor = bcolor;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+        public void RepeatCharacter(char ch, int count,bool newLine = true) 
+        {
+            string tempo = string.Empty;
+            tempo = tempo.PadLeft(count, ch);
+            if (newLine)
+            {
+                Console.WriteLine(tempo);
+            }
+            else
+            {
+                Console.Write(tempo);
+            }
+        }
+        public void RepeatCharacter(string str, int count)
+        {
+
+        }
 
     }
 }
--- Happy_CLI/Program.cs
+++ Happy_CLI/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Happy_CLI;
+ColorText colorText = new ColorText();
+colorText.writeLine("hello word",ConsoleColor.Green);
+colorText.writeLine("hellonima", ConsoleColor.Red, ConsoleColor.White);
+colorText.write("hello guy", ConsoleColor.Cyan);
+colorText.write("i am nima", ConsoleColor.Blue);
+colorText.write("mohammadi", ConsoleColor.Cyan,ConsoleColor.DarkGray);
+Console.WriteLine();
+colorText.RepeatCharacter('-', 20);
