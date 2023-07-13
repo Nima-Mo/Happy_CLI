afc71e8d45848c2d995829402939825d8d20e3a9
nima <mohammadi.ni@outlook.com>
2023-07-14 00:25:36 +0330

    Creating the columnOut method

--- Happy_CLI/OutStyle.cs
+++ Happy_CLI/OutStyle.cs
@@ -6,13 +6,69 @@ using System.Threading.Tasks;
 
 namespace Happy_CLI
 {
-    internal class OutStyle:Happy_CLI.AdvText
+    public class OutStyle:Happy_CLI.AdvText
     {
-        public OutStyle()
+        public OutStyle(){}
+        public void columnOut(string[]list,int row,int col,ConsoleColor color = ConsoleColor.White)
         {
-           
-        }
+            if (list.Length == 0)
+            {
+                return;
+            }
 
+            int counter = 0;
+            string tempo = string.Empty;
+            
+            for( int Col = 0;Col < col;Col++)
+            {
+                for(int Row = 0;Row < row;Row++)
+                {
+                    if(counter != list.Length)
+                    {
+                        tempo = string.Format("{0,-15}", list[counter]);
+                        this.write(tempo, color);
+                        counter++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+        public void columnOut(string[] list,int col, ConsoleColor color = ConsoleColor.White)
+        {
+            if(list.Length == 0)
+            {
+                return;
+            }
+
+            int counter = 0;
+            int Col = 0;
+            string tempo = string.Empty;
+
+            for (; counter < list.Length ;)
+            {
+                for(Col = 0; Col < col;Col++)
+                {
+                    if(counter != list.Length)
+                    {
+                        tempo = string.Format("{0,-15}", list[counter]);
+                        this.write(tempo, color);
+                        counter++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    
+                }
+                Col = 0;
+                Console.WriteLine();
+            }
+        }
 
     }
 }
--- Happy_CLI/Program.cs
+++ Happy_CLI/Program.cs
@@ -1,3 +1,12 @@
 ﻿using Happy_CLI;
 
 AdvText advtext = new AdvText();
+OutStyle outStyle = new OutStyle();
+string[] list =
+{
+    "1-nima","2-liana","3-parisa","4-nikoo","5-shima","6-parisa",
+    "7-nima","8-liana","9-parisa","10-nikoo","11-shima","12-parisa"
+};
+
+outStyle.columnOut(list,5);
+
