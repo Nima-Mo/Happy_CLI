bc1e81d5bef8afb40cffca9cf39792375e6f5843
MAHAN-PC <MAHAN-PC@DESKTOP-DJVBCRA>
2023-09-06 07:29:00 -0700

    test

--- Happy_CLI/Program.cs
+++ Happy_CLI/Program.cs
@@ -11,7 +11,8 @@ for (int i = 0; i < 10; i++)
 {
     adv.positionTextAt("  ",i,i+1,ConsoleColor.Green,ConsoleColor.Red);
 }
-adv.wink("wink",-1,ConsoleColor.Cyan,ConsoleColor.Red);
+adv.FlashingText("wink",-1,ConsoleColor.Cyan,ConsoleColor.Red);
 adv.newLine();
+
 adv.arrow("nima mohammadi",10,ConsoleColor.Cyan,ConsoleColor.Red,true);
 adv.arrow("nima mohammadi", 10, ConsoleColor.Cyan, ConsoleColor.Red,20,20, true);
