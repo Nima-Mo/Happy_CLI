b4d40b7c00a1548ef1522a29a4a748561573c17d
MAHAN-PC <MAHAN-PC@DESKTOP-DJVBCRA>
2023-09-10 10:23:51 -0700

    edit table class

--- Happy_CLI/Table.cs
+++ Happy_CLI/Table.cs
@@ -12,8 +12,8 @@ namespace Happy_CLI
         private string _description;
         private string _columns;
         private List<string> _rows;
-        private string[] _temp;
-        public Table() { }
+        private string[][] _temp;
+        private string[] _tempFormating;
         public Table(string tableName, string description, string columns, List<string> rows)
         {
             _tableName = tableName;
@@ -21,19 +21,52 @@ namespace Happy_CLI
             _columns = columns;
             _rows = rows;
         }
-        public string tableName {  get { return _tableName; } set { _tableName = value; } }
-        public string description { get { return _description; } set { _description = value; } }
-        public string columns { get { return _columns; } set { _columns = value; } }
-        public List<string> rows { get { return rows; } set { rows = value; } }
+        public string TableName 
+        {
+            get { return _tableName; } 
+            set { _tableName = value; } 
+        }
+        public string Description 
+        { 
+            get { return _description; } 
+            set { _description = value; } 
+        }
+        public string columns 
+        {
+            get { return _columns; } 
+            set { _columns = value; } 
+        }
+        public List<string> rows 
+        { 
+            get { return rows; } 
+            set { rows = value;} 
+        }
         protected void combine(List<string> rows,string columns) 
         {
             this._rows.Insert(0, columns);
         }
-        protected string[] spliter(string str)
+        protected void spliter(List<string>rows)
         {
-            this._temp = str.Split(',');
-            return this._temp;
+            this._temp = new string[rows.Count][];
+            for (int i = this._temp.GetLowerBound(0);i < this._temp.GetUpperBound(0);i++)
+            {
+                this._temp[i] = rows[i].Split(',');
+            }
         }
-
+        protected void formating(string[][]temp)
+        {
+           this._tempFormating = new string[temp.GetUpperBound(0)];
+            for(int i = 0 ;i < this._tempFormating.Length; i++)
+            {
+                this._tempFormating[i] = "|";
+            }
+           for (int i = temp.GetLowerBound(0);i < temp.GetUpperBound(0); i++)
+            {
+                for(int j = temp.GetLowerBound(1);j < temp.GetUpperBound(1);j++) 
+                {
+                    this._tempFormating[i] += string.Format("{0,15}|", temp[j]);
+                }
+            }
+        }
     }
 }
